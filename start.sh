b6384fb7e333ef02c9c6412d7e3e9093e0458686
adnan2002 <cshdjxbehsbxus@gmail.com>
2025-10-01 17:45:04 +0300

    changed some start.sh files here and there

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ echo "Waiting for database to be ready..."
 /app/wait-for.sh db:5432 -t 60 -- echo "Database is ready!"
 
 echo "Running database migrations..."
+source /app/app.env
 make migrateup
 
 echo "Starting application..."
